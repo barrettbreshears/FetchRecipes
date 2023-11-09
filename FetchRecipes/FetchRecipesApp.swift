1677231a71ea1831c7200348818960232b0b1b2e
Barrett Breshears <barrettbreshears@gmail.com>
2023-11-08 21:01:31 -0700

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Barrett Breshears on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
